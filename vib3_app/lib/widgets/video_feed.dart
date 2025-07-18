b45116a35541d2fc453f4d6f93ce615cab8e80e6
VIBE <vibe@vib3app.com>
2025-07-18 07:03:35 -0500

    Revert "Remove debug overlay from video feed"
    This reverts commit cd9c0ba682875e5c12d2368d1d5f90aa806fab59.

--- vib3_app/lib/widgets/video_feed.dart
+++ vib3_app/lib/widgets/video_feed.dart
@@ -32,6 +32,7 @@ import 'save_video_dialog.dart';
 // Import the better VIB3 themed components
 import 'video_feed_components/draggable/draggable_action_buttons.dart';
 import 'video_feed_components/state_manager.dart';
+import 'video_debug_overlay.dart';
 
 enum FeedType { forYou, following, friends }
 
@@ -1361,6 +1362,8 @@ class _VideoFeedState extends State<VideoFeed> with WidgetsBindingObserver {
               );
             },
           ),
+          // Add debug overlay for troubleshooting
+          const VideoDebugOverlay(),
         ],
       );
       },
